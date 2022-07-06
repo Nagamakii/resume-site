910d478611bbb227a3d0abb7de6faca14cb34100
Evan Quah <evanjquah@gmail.com>
2022-07-05 23:15:32 -0500

    First

---
+++ portfolio_website-STARTER/portfolio_website-STARTER/src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello! <b /> 
+        Welcome To My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        This is a test
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
